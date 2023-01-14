8c2fbfe91a3181c28741a404cee4b1721bc58340
edna chavarria <emchavarria1@gmail.com>
2023-01-14 09:26:09 +0000

    Update 4 files

--- src/extra.js
+++ src/extra.js
@@ -1,117 +1,103 @@
-function getWeather() {
-  let temperature = document.getElementById("temperature");
-  let description = document.getElementById("description");
-  let location = document.getElementById("location");
-
-  let api = "https://api.openweathermap.org/data/2.5/weather";
-  let apiKey = "c125eb344194a5a8a1f0e673edb26318";
-
-  location.innerHTML = "Locating...";
-
-  navigator.geolocation.getCurrentPosition(success, error);
-
-  function success(position) {
-    latitude = position.coords.latitude;
-    longitude = position.coords.longitude;
-
-    let url =
-      api +
-      "?lat=" +
-      latitude +
-      "&lon=" +
-      longitude +
-      "&appid=" +
-      apiKey +
-      "&units=imperial";
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        let temp = data.main.temp;
-        temperature.innerHTML = temp + "° F";
-        location.innerHTML =
-          data.name + " (" + latitude + "°, " + longitude + "°)";
-        description.innerHTML = data.weather[0].main;
-      });
-  }
-
-  function error() {
-    location.innerHTML = "Unable to retrieve your location";
-  }
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
 }
 
-getWeather();
-
-let now = new Date();
-let h3 = document.querySelector("h3");
-let date = now.getDate();
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let year = now.getFullYear();
-let milliseconds = now.getMilliseconds();
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-let day = days[now.getDay()];
-
-let months = [
-  "Jan",
-  "Feb",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "Aug",
-  "Sept",
-  "Oct",
-  "Nov",
-  "Dec"
+let days = [
+  "Sun", 
+  "Mon", 
+  "Tue", 
+  "Wed", 
+  "Thu", 
+  "Fri", 
+  "Sat"
 ];
-let month = months[now.getMonth()];
-let millisecond = milliseconds[now.getMilliseconds()];
-console.log(now.getMillisecond);
-console.log(now.getDate);
-console.log(now.getDay);
-console.log(now.getFullYear);
-console.log(now.getMonth);
-console.log(now.formatDate);
-h3.innerHTML = `${day} ${month} ${date}, ${hours}: ${minutes}, ${year}`;
-
-let inputval = document.querySelector("#cityinput");
-let btn = document.querySelector("#add");
-let city = document.querySelector("#cityoutput");
-let descrip = document.querySelector("#description");
-let temp = document.querySelector("#temp");
-let wind = document.querySelector("#wind");
-apik = "3045dd712ffe6e702e3245525ac7fa38";
-function convertion(val) {
-  return (val - 273).toFixed(2);
+let day = days[date.getDay()];
+return  `${day} ${hours}:${minutes}`;
 }
 
-btn.addEventListener("click", function () {
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      inputval.value +
-      "&appid=" +
-      apik
-  )
-    .then((res) => res.json())
+function displayTemperature(response) {
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+let dateElement = document.querySelector("#date");
+let iconElement = document.querySelector("#icon");
 
-    .then((data) => {
-      let nameval = data["name"];
-      let descrip = data["weather"]["0"]["description"];
-      let tempature = data["main"]["temp"];
-      let wndspd = data["wind"]["speed"];
-      city.innerHTML = `Weather of <span>${nameval}<span>`;
-      temp.innerHTML = `Temperature: <span>${convertion(tempature)} C</span>`;
-      description.innerHTML = `Sky Conditions: <span>${descrip}<span>`;
-      wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`;
-    })
+CelsiusTemperature = response.data.main.temp;
 
-    .catch((err) => alert("You entered Wrong city name"));
-});
-function submitted(event) {
+   temperatureElement.innerHTML = Math.round(CelsiusTemperature);
+   cityElement.innerHTML = response.data.name;
+   descriptionElement.innerHTML = response.data.weather[0].description;
+   humidityElement.innerHTML = response.data.main.humidity;
+   windElement.innerHTML = Math.round(response.data.wind.speed);
+   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+   iconElement.setAttribute (
+ "src", 
+ `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=bo47df7eab4a9c009f0343t0f7c9207d&units=metric'
+); 
+iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+function search(city) {
+  let apiKey = "c125eb344194a5a8a1f0e673edb26318"; 
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?q=${city}&appid=${apiKey}&units=metric`;
+ axios.get(apiUrl).then(displayTemperature);
+}
+ 
+function handleSubmit(event) {
+event.preventDefault();
+let cityInputElement = document.querySelector("#city-input");
+search(cityInputElement.value);
+}
+
+function displayFahrenheitTemperature(event) {
+event.preventDefault();
+let temperatureElement = document.querySelector("#temperature")
+let fahrenheitTemperature = (celsiusTemperature * 9) / 5 * 32;
+temperatureElement.innerHTML = math.round(fahrenheitTemperature);
+}
+
+let celsiusTemperature = null;
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-Link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+search("New York");
+
+
+
+celsiusLink.classList.remove("active");
+fahrenheitLink.classList.add("active");
+let fahrenheitTemperature = (celsiusTemperature * 9) / 5 * 32;
+temperatureElement.innerHTML = math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
   event.preventDefault();
-}
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let celsiusTemperature = (fahrenheitTemperature * 9) / 5 * 32;
+  temperatureElement.innerHTML = math.round(celsiusTemperature);
+  }
+  
+  
 
-f.addEventListener("submit", submitted);
+
+
+
+
+
+
+
+
