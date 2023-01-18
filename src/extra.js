08bea4c2fb5088d7af3c18c48214aff9cfe8806b
edna chavarria <emchavarria1@gmail.com>
2023-01-18 00:19:22 +0000

    Update extra.js

--- src/extra.js
+++ src/extra.js
@@ -106,12 +106,38 @@ function search(city) {
 }
 
 function handleSubmit(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#city-input");
-  search(cityInputElement.value);
+event.preventDefault();
+let cityInputElement = document.querySelector("#city-input");
+search(cityInputElement.value);
 }
+
+function displayFahrenheitTemperature(event) { 
+ event.preventDefault();
+ let temperatureElement = document.querySelector("#temperature");
+
+celsiusLink.classList.remove("active");
+fahrenheitLink.classList.add("active");
+let fahrenheitTemperature = (celsiusTemperature * 9) / 5 * 32;
+temperatureElement.innerHTML = math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+event.preventDefault();
+celsiusLink.classList.add("active");
+fahrenheitLink.classList.remove("active");
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-Link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
