0865c7902c7068db012e5407ee8ffdd027035191
edna chavarria <emchavarria1@gmail.com>
2023-01-19 02:47:38 +0000

    Update index.html and extra.js

--- src/extra.js
+++ src/extra.js
@@ -22,34 +22,27 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
- let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Wed"];
-  
-  return days[day];
-}
-
 function displayForecast(response) {
-  let forecast = response.data.daily;
-  
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+
   let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay) {
+  days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
       `
       <div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <div class="weather-forecast-date">${day}</div>
         <img
-          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+          src="http://openweathermap.org/img/wn/50d@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max}° </span>
-          <span class="weather-forecast-temperature-min"> ${forecastDay.temp.min}° </span>
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
         </div>
       </div>
   `;
